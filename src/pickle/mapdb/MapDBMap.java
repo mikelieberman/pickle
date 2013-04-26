fc3f03f57b6314db4b204a6674470c128a9434d2
mikelieberman <mike.d.lieberman@gmail.com>
2013-04-26 08:07:21 -0700

    - Added MapDBMap - Added NavigablePickleMap interface

--- src/pickle/mapdb/MapDBMap.java
+++ src/pickle/mapdb/MapDBMap.java
@@ -1,23 +1,26 @@
 package pickle.mapdb;
 
 import java.io.File;
-import java.util.AbstractMap;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.Map;
+import java.util.NavigableMap;
+import java.util.NavigableSet;
 import java.util.Set;
+import java.util.SortedMap;
 
 import org.mapdb.DB;
 import org.mapdb.DBMaker;
 
-import pickle.PickleMap;
+import pickle.NavigablePickleMap;
 
 /**
  * @author Michael Lieberman
  */
-public class MapDBMap<K, V> implements PickleMap<K, V> {
+public class MapDBMap<K, V> implements NavigablePickleMap<K, V> {
 
 	protected DB db;
-	protected Map<K, V> map;
+	protected NavigableMap<K, V> map;
 
 	public MapDBMap(String dir) {
 		this(dir, "map");
@@ -29,7 +32,10 @@ public class MapDBMap<K, V> implements PickleMap<K, V> {
 
 	public MapDBMap(String dir, String mapName, boolean clear) {
 		db = DBMaker.newFileDB(new File(dir)).writeAheadLogDisable().make();
-		map = db.getHashMap(mapName);
+		map = db.getTreeMap(mapName);
+		if (clear) {
+			clear();
+		}
 	}
 
 	@Override
@@ -102,6 +108,127 @@ public class MapDBMap<K, V> implements PickleMap<K, V> {
 		db.commit();
 		db.compact();
 		db.close();
+	}
+
+	@Override
+	public Map.Entry<K, V> lowerEntry(K key) {
+		return map.lowerEntry(key);
+	}
+
+	@Override
+	public K lowerKey(K key) {
+		return map.lowerKey(key);
+	}
+
+	@Override
+	public Map.Entry<K, V> floorEntry(K key) {
+		return map.floorEntry(key);
+	}
+
+	@Override
+	public K floorKey(K key) {
+		return map.floorKey(key);
+	}
+
+	@Override
+	public Map.Entry<K, V> ceilingEntry(K key) {
+		return map.ceilingEntry(key);
+	}
+
+	@Override
+	public K ceilingKey(K key) {
+		return map.ceilingKey(key);
+	}
+
+	@Override
+	public Map.Entry<K, V> higherEntry(K key) {
+		return map.higherEntry(key);
+	}
+
+	@Override
+	public K higherKey(K key) {
+		return map.higherKey(key);
+	}
+
+	@Override
+	public Map.Entry<K, V> firstEntry() {
+		return map.firstEntry();
+	}
+
+	@Override
+	public Map.Entry<K, V> lastEntry() {
+		return map.lastEntry();
+	}
+
+	@Override
+	public Map.Entry<K, V> pollFirstEntry() {
+		return map.pollFirstEntry();
+	}
+
+	@Override
+	public Map.Entry<K, V> pollLastEntry() {
+		return map.pollLastEntry();
+	}
+
+	@Override
+	public NavigableMap<K, V> descendingMap() {
+		return map.descendingMap();
+	}
+
+	@Override
+	public NavigableSet<K> navigableKeySet() {
+		return map.navigableKeySet();
+	}
+
+	@Override
+	public NavigableSet<K> descendingKeySet() {
+		return map.descendingKeySet();
+	}
+
+	@Override
+	public NavigableMap<K, V> subMap(K fromKey, boolean fromInclusive, K toKey,
+			boolean toInclusive) {
+		return map.subMap(fromKey, fromInclusive, toKey, toInclusive);
+	}
+
+	@Override
+	public NavigableMap<K, V> headMap(K toKey, boolean inclusive) {
+		return map.headMap(toKey, inclusive);
+	}
+
+	@Override
+	public NavigableMap<K, V> tailMap(K fromKey, boolean inclusive) {
+		return map.tailMap(fromKey, inclusive);
+	}
+
+	@Override
+	public SortedMap<K, V> subMap(K fromKey, K toKey) {
+		return map.subMap(fromKey, toKey);
+	}
+
+	@Override
+	public SortedMap<K, V> headMap(K toKey) {
+		return map.headMap(toKey);
+	}
+
+	@Override
+	public SortedMap<K, V> tailMap(K fromKey) {
+		return map.tailMap(fromKey);
+	}
+
+	@Override
+	public Comparator<? super K> comparator() {
+		return map.comparator();
+	}
+
+	@Override
+	public K firstKey() {
+		return map.firstKey();
+	}
+
+	@Override
+	public K lastKey() {
+		return map.lastKey();
 	}
 
 }

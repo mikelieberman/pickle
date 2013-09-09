fd8f96d58e501e288471eb1a4011a8d392727fd2
Michael Lieberman <Michael.Lieberman@jhuapl.edu>
2013-09-09 17:52:00 -0400

    Version 0.1.0: * Updated NavigablePickleMap to extend PickleMap and Map * Reorganized source to a more Maven-esque layout * Got rid of Eclipse cruft * Source cleanup to avoid warnings

---
+++ src/main/java/pickle/NavigablePickleMap.java
@@ -0,0 +1,10 @@
+package pickle;
+
+import java.util.NavigableMap;
+
+/**
+ * @author Michael Lieberman
+ */
+public interface NavigablePickleMap<K, V> extends PickleMap<K, V>, NavigableMap<K, V> {
+
+}
---
+++ src/main/java/pickle/PickleMap.java
@@ -0,0 +1,21 @@
+package pickle;
+
+import java.util.Map;
+
+/**
+ * @author Michael Lieberman
+ */
+public interface PickleMap<K, V> extends Map<K, V> {
+
+	/**
+	 * Flushes any in-memory keys/values to the backing store.
+	 */
+	public void flush();
+
+	/**
+	 * Flushes any in-memory data and closes
+	 * the backing store connection.
+	 */
+	public void close();
+
+}
